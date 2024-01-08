e2f4dbfefc177b020aed65e57c679c55e98a7856
Alexander <asederado@gmail.com>
2024-01-08 22:12:07 +0100

    refactor access member

--- src/EntradaTrabajador.tsx
+++ src/EntradaTrabajador.tsx
@@ -15,11 +15,11 @@ const EntradaTrabajador = () => {
 
     return (
         <div>
-            <h1>Entrada Trabajador</h1>
+            <h1>Acceso Trabajador</h1>
             {trabajadorCode !== "" && trabajadores.find((trabajador) => trabajador.code === trabajadorCode) && <h2 className="ms-4">{trabajadores.find((trabajador) => trabajador.code === trabajadorCode)?.name}</h2>}
             <div className="d-flex flex-row justify-content-center ms-4">
                 <input value={trabajadorCode} type="number" onChange={(e) => setTrabajadorCode(e.target.value)} className="form-control mb-2 mt-2 ml-4" placeholder="Código del trabajador" />
-                <button disabled={!(trabajadorCode !== "" && trabajadores.find((trabajador) => trabajador.code === trabajadorCode))} className="btn btn-primary" onClick={() => navigate("/trabajador?code=" + trabajadorCode)}>Entrada</button>
+                <button disabled={!(trabajadorCode !== "" && trabajadores.find((trabajador) => trabajador.code === trabajadorCode))} className="btn btn-primary" onClick={() => navigate("/trabajador?code=" + trabajadorCode)}>Acceso</button>
             </div>
         </div>
     )
